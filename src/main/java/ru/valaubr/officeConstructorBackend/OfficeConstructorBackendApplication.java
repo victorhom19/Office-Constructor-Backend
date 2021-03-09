a252ecfc7a53488d71c0ef787032237f8a1d059b
Valaubr <stalker039906@gmail.com>
2021-03-09 14:13:03 +0300

    spring initi. starter pattenr.

---
+++ src/main/java/ru/valaubr/officeConstructorBackend/OfficeConstructorBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.valaubr.officeConstructorBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeConstructorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeConstructorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/valaubr/officeConstructorBackend/OfficeConstructorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.valaubr.officeConstructorBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeConstructorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
